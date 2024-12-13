99435bca73d1a1e5b621c43fb17f6175032d3a1e
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-13 21:56:56 +0530

    Array_map

--- Array.js
+++ Array.js
@@ -1,3 +1,10 @@
-let arr1 = [10,20,30,40];
-console.log(arr1);
-console.log(arr1[1]);
+// let arr1 = [10,20,30,40];
+// console.log(arr1);
+// console.log(arr1[1]);
+
+let  arr2 = [100,20,30,400,50];
+let arr_new = arr2.map((element, index)=>{
+    return element*100;
+});
+console.log(arr_new);
+
