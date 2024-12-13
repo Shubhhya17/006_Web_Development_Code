8ab64184758e345133d1ea2ca00b0849cf853651
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-13 22:03:41 +0530

    Filter()

--- Array.js
+++ Array.js
@@ -2,9 +2,22 @@
 // console.log(arr1);
 // console.log(arr1[1]);
 
-let  arr2 = [100,20,30,400,50];
-let arr_new = arr2.map((element, index)=>{
-    return element*100;
+// let  arr2 = [100,20,30,400,50];
+// let arr_new = arr2.map((element, index)=>{
+//     return element*100;
+// });
+// console.log(arr_new);
+
+//! reduce
+// let arr9 = [10,20,30,40,50];
+// let arr3 = arr9.reduce((firstElement, nextElement)=>{
+//     console.log(arr3.includes(30));
+// })
+
+//!filter 
+let arr6 = [10,20,30,40,50,60];
+let res1 = arr6.filter((element, index)=>{
+    return element>=30;
 });
-console.log(arr_new);
+console.log(res1);
 
